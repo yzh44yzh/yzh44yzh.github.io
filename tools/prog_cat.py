ee61d71012508346c5a3c7435e49bc423e323197
Yuriy Zhloba <y_zhloba@wargaming.net>
2018-04-07 19:25:06 +0300

    generate prog_cat pages

--- tools/prog_cat.py
+++ tools/prog_cat.py
@@ -1,64 +1,69 @@
 #!/usr/bin/env python3
 
 # TODO
-# author page, also active link for this page
-# save all pages to files
-# check navigation works
-# rename index.html to archive.html, season_00.html to index.html, fix navigation
-# fix navigation for ./post/*.html pages, links to archive.html instead of index.html
+# - rename index.html to archive.html, season_00.html to index.html, check navigation again
+# - fix navigation for ./post/*.html pages, links to archive.html instead of index.html
 
 import pystache
 
 model = [
     {
         'input': 'data/prog_cat/season_00.txt',
-        'output': 'season_00.html',
-        'template': 'data/prog_cat/template.html',
+        'output': 'season_00.html', # TODO index.html
         'bindings': {
-            'title': 'Сезон #00.',
-            'date': '23 апреля 2016'
+            'title': 'Сезон #00',
+            'date': '23 апреля 2016',
+            'author_page': False
         }
     },
     {
         'input': 'data/prog_cat/season_01.txt',
         'output': 'season_01.html',
-        'template': 'data/prog_cat/template.html',
         'bindings': {
-            'title': 'Сезон #01.',
-            'date': '4 августа 2016'
+            'title': 'Сезон #01',
+            'date': '4 августа 2016',
+            'author_page': False
         }
     },
     {
         'input': 'data/prog_cat/season_02.txt',
         'output': 'season_02.html',
-        'template': 'data/prog_cat/template.html',
         'bindings': {
-            'title': 'Сезон #02.',
-            'date': '2 июня 2017'
+            'title': 'Сезон #02',
+            'date': '2 июня 2017',
+            'author_page': False
         }
     },
     {
         'input': 'data/prog_cat/season_03.txt',
         'output': 'season_03.html',
-        'template': 'data/prog_cat/template.html',
         'bindings': {
-            'title': 'Сезон #03.',
-            'date': '24 ноября 2017'
+            'title': 'Сезон #03',
+            'date': '24 ноября 2017',
+            'author_page': False
         }
     },
     {
         'input': 'data/prog_cat/season_04.txt',
         'output': 'season_04.html',
-        'template': 'data/prog_cat/template.html',
         'bindings': {
-            'title': 'Сезон #04.',
-            'date': '28 марта 2018'
+            'title': 'Сезон #04',
+            'date': '28 марта 2018',
+            'author_page': False
+        }
+    },
+    {
+        'input': 'data/prog_cat/author.html',
+        'output': 'author.html',
+        'bindings': {
+            'title': 'Авторы',
+            'author_page': True
         }
     }
 ]
 
 navigation = []
-for data in model:
+for data in model[0:-1]:
     navigation.append({
         'href': data['output'],
         'text': data['bindings']['title']
@@ -66,13 +71,16 @@ for data in model:
 
 
 def make_page(index, data):
-    data['bindings']['episodes'] = get_episodes(data['input'], index)
+    if data['bindings']['author_page']:
+        data['bindings']['author_content'] = get_file(data['input'])
+    else:
+        data['bindings']['episodes'] = get_episodes(data['input'], index)
+
     for item in navigation:
         item['active'] = item['text'] == data['bindings']['title']
     data['bindings']['navigation'] = navigation
-    template = get_template(data['template'])
-    page = pystache.render(template, data['bindings'])
-    print(page)
+    content = pystache.render(template, data['bindings'])
+    save_file(data['output'], content)
 
 
 def get_episodes(input_file, season_number):
@@ -83,7 +91,7 @@ def get_episodes(input_file, season_number):
     for line in lines:
         line = line.strip()
         if line:
-            content += '<p>' + line[0:-1] + '</p>\n'
+            content += '<p>' + line + '</p>\n'
         else:
             if content:
                 contents.append(content)
@@ -94,11 +102,21 @@ def get_episodes(input_file, season_number):
             'e_title': 'Эпизод #{:02X}'.format(i + season_number * 16),
             'e_content': content
         })
-    return episodes[0:2] # TODO temporary slicing
+    return episodes
 
 
-def get_template(template_file):
-    fd = open(template_file, 'r')
+def get_file(file):
+    fd = open(file, 'r')
     return fd.read()
 
-make_page(4, model[4])
+
+def save_file(file, content):
+    print("save {}".format(file))
+    fd = open(file, 'w')
+    fd.write(content)
+
+
+template = get_file('data/prog_cat/template.html')
+
+for i, item in enumerate(model):
+    make_page(i, item)
