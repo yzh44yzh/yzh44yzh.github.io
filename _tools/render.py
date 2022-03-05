a7d42e7649f34af1c56b245728afcccd49c316a9
Yuriy Zhloba <y_zhloba@wargaming.net>
2022-03-05 20:38:06 +0300

    render courses.html from template

--- _tools/render.py
+++ _tools/render.py
@@ -1,28 +1,37 @@
-import os
-import chevron  # https://github.com/noahmorrison/chevron
+# https://github.com/noahmorrison/chevron
+# https://mustache.github.io/mustache.5.html
+# https://mustache.github.io/mustache.1.html
 
+import os
+import chevron
 
 def render_all():
     base_tpl_file = open('_templates/base.tpl.html')
+    base_tpl = base_tpl_file.read()
+    base_tpl_file.close()
+    
     pages = [
-        ('index', 'Главная')
+        ('index', 'Главная'),
+        ('courses', 'Учебные курсы')
     ]
     for page in pages:
-        render_page(page, base_tpl_file)
+        render_page(page, base_tpl)
 
 
-def render_page(page, base_tpl_file):
+def render_page(page, base_tpl):
     print(f'Render {page}')
     (page, page_name) = page
     page_tpl_file = open(f'_templates/{page}.tpl.html')
     content = page_tpl_file.read()
+    page_tpl_file.close()
     data = {
         'page_name': page_name,
         'content': content
     }
-    content = chevron.render(base_tpl_file, data)
+    content = chevron.render(base_tpl, data)
     page_file = open(f'{page}.html', 'w')
     page_file.write(content)
+    page_file.close()
 
     
 if __name__ == '__main__':
